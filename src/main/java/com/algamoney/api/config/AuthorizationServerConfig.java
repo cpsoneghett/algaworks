8a3c4b97427c0057f5a706f0a0c252efa6e8ed2a
Christiano <cpsoneghett@gmail.com>
2019-09-26 14:39:44 -0300

    6.5 - Modificacao do metodo accessTokenConverter: inserido @Bean e alterada politica de acesso do metodo para public

--- src/main/java/com/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/algamoney/api/config/AuthorizationServerConfig.java
@@ -43,7 +43,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.authenticationManager( authenticationManager );
 	}
 
-	private JwtAccessTokenConverter accessTokenConverter() {
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
 		accessTokenConverter.setSigningKey( "algaworks" );;
 		return accessTokenConverter;
