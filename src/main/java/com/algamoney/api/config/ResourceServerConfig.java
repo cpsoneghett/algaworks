fe85000d6cbd719dde3dde4a1847264035ea6794
Christiano <cpsoneghett@gmail.com>
2019-10-16 01:53:34 -0300

    6.12 - Primeiro passo: adicionando autorizacao para roles de Categoria

--- src/main/java/com/algamoney/api/config/ResourceServerConfig.java
+++ src/main/java/com/algamoney/api/config/ResourceServerConfig.java
@@ -1,21 +1,21 @@
 package com.algamoney.api.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.servlet.WebSecurityEnablerConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
 
 @Configuration
+@EnableWebSecurity
 @EnableResourceServer
+@EnableGlobalMethodSecurity
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
@@ -32,6 +32,12 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	public void configure( ResourceServerSecurityConfigurer resources ) throws Exception {
 
 		resources.stateless( true );
+	}
+	
+	@Bean
+	public MethodSecurityExpressionHandler createExpressionHandler() {
+		
+		return new OAuth2MethodSecurityExpressionHandler();
 	}
 
 }
--- src/main/java/com/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/algamoney/api/resource/CategoriaResource.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,11 +33,13 @@ public class CategoriaResource {
 	private ApplicationEventPublisher publisher;
 
 	@GetMapping
+	@PreAuthorize( "hasAuthority('ROLE_PESQUISAR_CATEGORIA')" )
 	public List<Categoria> listar() {
 		return categoriaRepository.findAll();
 	}
 
 	@PostMapping
+	@PreAuthorize( "hasAuthority('ROLE_CADASTRAR_CATEGORIA')" )
 	public ResponseEntity<Categoria> criar( @Valid @RequestBody Categoria categoria, HttpServletResponse response ) {
 		Categoria categoriaSalva = categoriaRepository.save( categoria );
 
@@ -46,6 +49,7 @@ public class CategoriaResource {
 	}
 
 	@GetMapping( "/{id}" )
+	@PreAuthorize( "hasAuthority('ROLE_PESQUISAR_CATEGORIA')" )
 	public ResponseEntity<Categoria> buscarPorId( @PathVariable Long id ) {
 
 		Optional<Categoria> categoriaObtida = categoriaRepository.findById( id );
